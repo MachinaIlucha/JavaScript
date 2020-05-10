bf16ad93ab4e5d04aa330931e569863dfd0b479a
MachinaIlucha <44270738+MachinaIlucha@users.noreply.github.com>
2020-05-10 22:52:25 +0300

    Add files via upload

---
+++ lesson4/task5/index.js
@@ -0,0 +1,14 @@
+let m = 4;
+let n = 37;
+let result = 0;
+
+for (var i = m; i <= n; i++) {
+	if (i % 2 == 0 && i % 4 == 1 ) 
+		result += i;
+	else if (i % 3 == 0) 
+		result -= i;
+	else if (i % 4 == 0)
+		result *= i;
+	else if (i % 5 == 0) 
+		console.log(i); 
+}
