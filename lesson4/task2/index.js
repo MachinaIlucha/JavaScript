d5dcf889af02d7ee9de49fd6c81f864a3e70c96c
MachinaIlucha <44270738+MachinaIlucha@users.noreply.github.com>
2020-05-10 21:52:28 +0300

    Add files via upload

--- lesson4/task2/index.js
+++ lesson4/task2/index.js
@@ -1,8 +1,11 @@
+let m = 10
+const n = 20;
+
 let result = 1;
 
-for (let m = 10; m <= 20; m++) {
-	if (m % 2 === 1){
-		result *= m;
+for (var i = m; i <= n; i++) {
+	if (i % 2 === 1){
+		result *= i;
 	}
 }
 
