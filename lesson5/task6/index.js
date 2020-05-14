6be6d8abe71431d9d18c7ec98c8fc4a49f733176
MachinaIlucha <44270738+MachinaIlucha@users.noreply.github.com>
2020-05-14 16:11:26 +0300

    Add files via upload

--- lesson5/task6/index.js
+++ lesson5/task6/index.js
@@ -1,8 +1,6 @@
-getPrimes(17);
-
 function getPrimes(a)
 {
-   for (var i = 2; i <= a; i++) {
+   for (var i = 1; i <= a; i++) {
    	for (var j = 2; j < i; j++) {
    		if (i % j === 0)
    			i++;
