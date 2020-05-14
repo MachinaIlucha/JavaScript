f4b99df8a667e0ead832201ee6867a4a2da44952
MachinaIlucha <44270738+MachinaIlucha@users.noreply.github.com>
2020-05-14 21:12:46 +0300

    Add files via upload

---
+++ lesson6/task2/index.js
@@ -0,0 +1,11 @@
+function getSum(arr){
+    if(!Array.isArray(arr))
+        return null;
+
+    let sum = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
