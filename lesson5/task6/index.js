81a5383b7e8154574266545b06a89efc1ff00d4a
MachinaIlucha <44270738+MachinaIlucha@users.noreply.github.com>
2020-05-14 14:15:45 +0300

    Add files via upload

--- lesson5/task6/index.js
+++ lesson5/task6/index.js
@@ -2,19 +2,11 @@ getPrimes(17);
 
 function getPrimes(a)
 {
-    var store  = [], i, j, primes = [];
-    for (i = 2; i <= a; ++i) 
-    {
-        if (!store [i]) 
-          {
-            primes.push(i);
-            for (j = i << 1; j <= a; j += i) 
-            {
-                store[j] = true;
-            }
-        }
-    }
-    for (var i = 0; i < primes.length; i++) {
-    	console.log(primes[i]);
-    }
+   for (var i = 2; i <= a; i++) {
+   	for (var j = 2; j < i; j++) {
+   		if (i % j === 0)
+   			i++;
+   	}
+   	console.log(i);
+   }
 }
