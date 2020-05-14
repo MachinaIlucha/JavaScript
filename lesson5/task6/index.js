32dcaf6d11d11fb14b2832709a0e4aaa5b614df5
MachinaIlucha <44270738+MachinaIlucha@users.noreply.github.com>
2020-05-14 12:37:03 +0300

    Add files via upload

--- lesson5/task6/index.js
+++ lesson5/task6/index.js
@@ -1,6 +1,11 @@
+getPrimes(98);
+
 function getPrimes(a){
-	for (let i = 2; i <= a; i++) { 
-		for (let j = 2; j < i; j++) { 
-    		if (i % j != 0) 
-    			return i;
+	nextPrime:
+	for (let i = 2; i <= a; i++) { // Для всех i...
+  		for (let j = 2; j < i; j++) { // проверить, делится ли число..
+    		if (i % j == 0) continue nextPrime; // не подходит, берём следующее
+  		}
+  		console.log(i);
+	}
 }
