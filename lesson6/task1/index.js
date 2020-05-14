5e1a63b9012ce6d87e707114a1e0b88b5730c935
MachinaIlucha <44270738+MachinaIlucha@users.noreply.github.com>
2020-05-14 20:52:12 +0300

    Add files via upload

--- lesson6/task1/index.js
+++ lesson6/task1/index.js
@@ -1,9 +1,9 @@
 function getArrayBounds(arr){
-    if(!arr.isArray())
-        return null
-    let c = [];
-    c[0] = arr.length;
-    c[1] = arr[0];
-    c[2] = arr[arr.length - 1];
-    return c;
+    if(arr.isArray()){
+        let c = [];
+        c[0] = arr.length;
+        c[1] = arr[0];
+        c[2] = arr[arr.length - 1];
+        return c;
+    } else return null;
 }
