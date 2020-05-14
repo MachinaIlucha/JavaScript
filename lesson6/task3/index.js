5506fd04cbdd0db4a74a9507f0be1dc09201dc54
MachinaIlucha <44270738+MachinaIlucha@users.noreply.github.com>
2020-05-14 21:21:44 +0300

    Add files via upload

--- lesson6/task3/index.js
+++ lesson6/task3/index.js
@@ -2,7 +2,7 @@ function getSpecialNumbers(m,n){
     let arr = [];
     let c = 0;
     for (let i = m; i <= n; i++) {
-        if(i % 3 === n){
+        if(i % 3 === 0){
             arr[c] = i;
             c++;
         }
