054d0c979e6eb5d1019e225ac513585a961c800d
MachinaIlucha <44270738+MachinaIlucha@users.noreply.github.com>
2020-05-15 11:06:21 +0300

    Add files via upload

--- lesson6/task10/index.js
+++ lesson6/task10/index.js
@@ -1,5 +1,13 @@
-function cloneArr(arr){
+function checker(arr){
     if(!Array.isArray)
-        return null; 
-    return arr.slice();
+        return null;
+    let max = arr[0];
+    let min = arr[0];
+    arr.forEach(element => {
+        if(element > max)
+            max = element
+        else if(element < min)
+            min = element;
+    });
+    return (max + min) > 1000 ? true : false;
 }
