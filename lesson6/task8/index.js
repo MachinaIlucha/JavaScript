50391193384c11bfbadf176612873e58de86e42d
MachinaIlucha <44270738+MachinaIlucha@users.noreply.github.com>
2020-05-15 10:39:44 +0300

    Add files via upload

--- lesson6/task8/index.js
+++ lesson6/task8/index.js
@@ -1,8 +1,18 @@
 function increaseEvenEl(arr, delta){
     if (!Array.isArray(arr)) 
         return null;
+
+    let i = 0;
+
     arr.forEach(element => {
-        if(element % 2 === 0)
-            element += delta;
+        if(element % 2 === 0){
+            arr[i] = element + delta;  
+            i++;
+        } else {
+            arr[i] = element
+            i++;
+        }
+                              
     });
+    return arr;
 }
