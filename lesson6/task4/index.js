f4bb3ddbe6e1c6cb9b39fe4b72ee6ff505a8df91
MachinaIlucha <44270738+MachinaIlucha@users.noreply.github.com>
2020-05-15 09:54:37 +0300

    Add files via upload

--- lesson6/task4/index.js
+++ lesson6/task4/index.js
@@ -1,5 +1,10 @@
 function swap(arr){
-    let a = arr;
+    let a = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        a[i] = arr[i]; 
+    }
+
     [a[a.length - 1], a[0]] = [a[0], a[a.length - 1]];
     return a;
 }
