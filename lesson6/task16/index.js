4c4933949da5ef29d9492493446b61bfdb1c5754
MachinaIlucha <44270738+MachinaIlucha@users.noreply.github.com>
2020-05-15 12:19:03 +0300

    Add files via upload

---
+++ lesson6/task16/index.js
@@ -0,0 +1,17 @@
+function uniqueCount(arr){
+    if(!Array.isArray(arr))
+        return null;
+
+    var seen = {};
+    var out = [];
+    var len = a.length;
+    var j = 0;
+    for(var i = 0; i < len; i++) {
+         var item = a[i];
+         if(seen[item] !== 1) {
+               seen[item] = 1;
+               out[j++] = item;
+         }
+    }
+    return out.length;
+}
