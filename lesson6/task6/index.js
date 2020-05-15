a7750ced70885b2456106d5c5497a1c38bdb0b57
MachinaIlucha <44270738+MachinaIlucha@users.noreply.github.com>
2020-05-15 10:11:18 +0300

    Add files via upload

---
+++ lesson6/task6/index.js
@@ -0,0 +1,14 @@
+function squareArray(arr){
+    if(!Array.isArray(arr))
+        return null;
+    let arrTo = [];
+    return copyArray(arr, arrTo);
+}
+
+function copyArray(arrFrom, arrTo){
+    let i = 0;
+    arrFrom.forEach(element => {
+        arrTo[i] = element;
+        i++;
+    });
+}
