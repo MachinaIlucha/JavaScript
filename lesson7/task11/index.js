53e84dacb3754bf2928fc8ded14a623056bf51a3
MachinaIlucha <44270738+MachinaIlucha@users.noreply.github.com>
2020-05-18 17:22:13 +0300

    Add files via upload

--- lesson7/task11/index.js
+++ lesson7/task11/index.js
@@ -1,5 +1,7 @@
 const arrAverage = arr => {
     if(!Array.isArray(arr)) return null;
-    let sum = values.reduce((previous, current) => current += previous);
-    return sum / values.length;
+    let sum = arr.reduce((previous, current) => current += previous);
+    return sum / arr.length;
 }
+
+//console.log(arrAverage([2, 56, 3, 41, 0, 4, 100, 23]));
