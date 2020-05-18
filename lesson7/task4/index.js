3d236ce2ec1e12c9c2a43c790566c5ce3c9f248d
MachinaIlucha <44270738+MachinaIlucha@users.noreply.github.com>
2020-05-18 15:52:32 +0300

    Add files via upload

--- lesson7/task4/index.js
+++ lesson7/task4/index.js
@@ -1,3 +1,3 @@
-const getMessagesForPassedStudents = (allStudents, failedStudents) => allStudents
+const getMessagesForBestStudents = (allStudents, failedStudents) => allStudents
         .filter(name => !failedStudents.includes(name))
         .map(name => 'Good job, ' + name);;
